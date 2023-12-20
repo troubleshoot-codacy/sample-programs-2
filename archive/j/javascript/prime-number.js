c6dd23cc305ed86c789484927978fd23be0ecf54
Alejandro Rizzo <alerizzo@gmail.com>
2023-12-20 12:36:59 +0000

    Add isNotPrime

--- archive/j/javascript/prime-number.js
+++ archive/j/javascript/prime-number.js
@@ -1,21 +1,24 @@
 // test 1
 const isPrime = (number) => {
-    if(number <= 1)
-      return false
+  if(number <= 1)
+    return false
 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-      if (number % i == 0) {
-        return false;
-      }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i == 0) {
+      return false;
     }
-    return number > 1;
-  };
+  }
   
-  const input = process.argv[2];
-  let number = Number(input)
-  
-  if (input !== '' && Number.isInteger(number) && number >= 0) {
+  return number > 1;
+};
+
+const isNotPrime = (number) => !isPrime(number);
+
+const input = process.argv[2];
+let number = Number(input)
+
+if (input !== '' && Number.isInteger(number) && number >= 0) {
     isPrime(input) ? console.log("prime") : console.log("composite");
   } else {
     console.log("Usage: please input a non-negative integer")
-  }
+}
