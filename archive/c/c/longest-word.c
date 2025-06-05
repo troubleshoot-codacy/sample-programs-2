8f95107bcc482e4a5f10e8250810cc5544457d91
andrzej.janczak <andrzej.janczak@codacy.com>
2025-06-05 10:49:21 +0200

    Revert "tests"
    This reverts commit 1e203732ed4fcaf90c35f228dc92d91d0c07b4bc.

--- archive/c/c/longest-word.c
+++ archive/c/c/longest-word.c
@@ -1,60 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-/**
- * Prints error message and exits the program
- */
-void error() {
+void error(){
     printf("Usage: please provide a string\n");
     exit(0);
 }
-
-/**
- * Finds the length of the longest word in a given string
- * Words are separated by spaces, commas, newlines, or tabs
- */
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        error();
-    }  
-
-
-    if (argv[1] == NULL) {
-        error();
-    } 
-    
-    size_t input_len = strnlen(argv[1], INT_MAX);
-    if (input_len <= 0) {
-        error();
-    } 
-
- /// The maximum length of a word is initialized to -1
-    
- /// edit 
-
-
-    // Initialize max length to -1 to handle cases with no valid words    
-    // This allows us to return 0 if no words are found 
-    // and avoids confusion with a valid word of length 0
-    
-     
-    int max = -1;
-    char* word = strtok(argv[1], " ,\n\t");
-     
-    if (word == NULL) {
+int main(int argc, char *argv[])
+{
+    if(argc<2){
         error();
     }
-    
+    if(strlen(argv[1])<=0){
+        error();
+    }
+    int max = -1;
+    char* word = strtok(argv[1], " ,\n\t");
     while (word != NULL) {
-        size_t len = strnlen(word, INT_MAX);
-        if (len > (size_t)max) {
-            max = (int)len;
+        int len = strlen(word);
+        if(len>max){
+            max = len;
         }
         word = strtok(NULL, " ,\n\t");
     }
 
-    printf("%d", max);
+    printf("%d",max);
+      
+    
     return 0;
 }
